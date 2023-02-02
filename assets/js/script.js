78c49e090a7e13dcdd737767cc3f9792098e2cd1
camilafbc <camilafernanda914@gmail.com>
2023-02-02 20:50:42 -0300

    correção de lógica e estilo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,84 @@
 const input = document.getElementById("input-display")
+const valorAnterior = document.getElementById("valor-anterior")
 const botoes = document.querySelectorAll(".btn-key")
 const clearBtn = document.getElementById("clear-btn")
 const resultBtn = document.getElementById("result-btn")
-const operadores = document.querySelectorAll("operator-btn")
+const operadores = document.querySelectorAll(".operator-btn")
+let operador = ""
 
 clearBtn.addEventListener('click', function(){
     input.value = ""
+    valorAnterior.innerText = ""
+    operador = ""
 })
 
-botoes.forEach(function(btn){
-    btn.addEventListener('click', function(ev){
-        if(input.value === "0"){
-            input.value = btn.value
-        } else {
-            input.value += btn.value
-        }
+botoes.forEach(btn => {
+    btn.addEventListener('click', () => {
+      if(btn.value === "." && input.value.includes(".")){
+        return
+      } else {
+        input.value = input.value + btn.value
+      }
     })
 })
 
-operadores.forEach(function(operador){
-    operador.addEventListener('click', function(){
-        // if(input.value !== ""){
-        //     // let operacao = operador.value
-        //     input.value += operacao
-        //     // return operacao
-        // }
-        input.value += operador.value
-    })
-})
 
-resultBtn.addEventListener('click', calculate)
-
-function calculate(operador){
-    const getCount = input.value
-    let teste = getCount.split(operador)
-    console.log(teste)
-}
-
-
-// TESTE COM MÉTODO SPLIT
-// function calculate(){
-//     const getCount = input.value
-//     let teste = getCount.split(operador)
-
-//     let soma = parseFloat(teste[0]) + parseFloat(teste[1])
-//     teste[0] = soma
-//     console.log(soma)
-//     console.log(teste)
-
-//     // switch (operador) {
-//     //     case "+":
-//     //         let resultado = parseFloat(teste[0]) + parseFloat(teste[1])
-//     //         return resultado
-//     //         console.log(resultado)
-//     //         break;
+operadores.forEach(op => {
+    op.addEventListener('click', () => {
+        
+        
+       if(input.value === "") return
+       else if(operador ==""){
+        operador = op.value
+        input.value = input.value + operador
+       }
+    //    } else if(operador == ""){
+    //     operador = op.value
+    //     // valorAnterior.innerHTML = `${input.value + op.value}`
+    //     input.value = input.value + op.value
+    //     } 
+    //     else if(input.value.includes(operador !== "")){
+    //     return} 
+    //     else {
+    //     calcular()
+    //     input.value = input.value + op.value
+    //     operador = op.value
+    //    }
     
-//     //     default:
-//     //         break;
-//     // }
-// }
+
+    })
+})
+
+resultBtn.addEventListener('click', calcular)
+
+function calcular(){
+
+    let numeros = input.value.split(operador)
+
+    // if (numeros[1] == "") {
+    //     input.value = input.value + '0';
+    //     numeros[1] = 0;
+    // }
+
+    switch (operador) {
+        case "+":
+            input.value = parseFloat(numeros[0]) + parseFloat(numeros[1])
+            operador = ""
+            break;
+
+        case "-":
+            input.value = parseFloat(numeros[0]) - parseFloat(numeros[1])
+            operador = ""
+            break;
+
+        case "x": 
+            input.value = parseFloat(numeros[0]) * parseFloat(numeros[1])
+            operador = ""
+            break;
+
+        case "/":
+            input.value = parseFloat(numeros[0]) / parseFloat(numeros[1])
+            operador = ""
+            break;   
+    }
+}
