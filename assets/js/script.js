81e86cb24754646962ac30be41e28d94af5f9020
camilafbc <camilafernanda914@gmail.com>
2023-02-04 16:20:05 -0300

    correção e adição de botão apagar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,56 @@
 const input = document.getElementById("input-display")
-const valorAnterior = document.getElementById("valor-anterior")
 const botoes = document.querySelectorAll(".btn-key")
+const clearAllBtn = document.getElementById("clear-all-btn")
 const clearBtn = document.getElementById("clear-btn")
 const resultBtn = document.getElementById("result-btn")
 const operadores = document.querySelectorAll(".operator-btn")
 let operador = ""
 
-clearBtn.addEventListener('click', function(){
-    input.value = ""
-    valorAnterior.innerText = ""
-    operador = ""
-})
 
 botoes.forEach(btn => {
     btn.addEventListener('click', () => {
-      if(btn.value === "." && input.value.includes(".")){
-        return
-      } else {
+
+      let numeros = input.value.split(operador)
+
+      if(btn.value === "." && numeros[0].includes(".") && numeros[1].includes(".")) return
+      else {
         input.value = input.value + btn.value
       }
     })
 })
 
-
 operadores.forEach(op => {
-    op.addEventListener('click', () => {
-        
-        
+    op.addEventListener('click', () => {  
        if(input.value === "") return
        else if(operador ==""){
         operador = op.value
         input.value = input.value + operador
        }
-    //    } else if(operador == ""){
-    //     operador = op.value
-    //     // valorAnterior.innerHTML = `${input.value + op.value}`
-    //     input.value = input.value + op.value
-    //     } 
-    //     else if(input.value.includes(operador !== "")){
-    //     return} 
-    //     else {
-    //     calcular()
-    //     input.value = input.value + op.value
-    //     operador = op.value
-    //    }
-    
-
     })
+})
+
+clearBtn.addEventListener('click', () => {
+    if(input.value.includes(operador!=="")){
+        input.value = input.value.slice(0,-1)
+    } else {
+        input.value = input.value.slice(0,-1)
+        operador = ""
+    }
+})
+
+clearAllBtn.addEventListener('click', () => {
+    input.value = ""
+    operador = ""
 })
 
 resultBtn.addEventListener('click', calcular)
 
 function calcular(){
 
-    let numeros = input.value.split(operador)
+    const numeros = input.value.split(operador)
 
-    // if (numeros[1] == "") {
-    //     input.value = input.value + '0';
-    //     numeros[1] = 0;
-    // }
+
+    if(numeros[1] === "") return
 
     switch (operador) {
         case "+":
@@ -79,6 +71,6 @@ function calcular(){
         case "/":
             input.value = parseFloat(numeros[0]) / parseFloat(numeros[1])
             operador = ""
-            break;   
+            break; 
     }
 }
